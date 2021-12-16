a2a62dd4df555534139d131f8891c183c3bb5232
Jennifer Cobb <50578967+houleyemballo@users.noreply.github.com>
2021-12-16 17:40:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Pneumonia Image Classification Using Deep Learning
+
+![Normal_vs_Pneumonia_Xray](images/norm_pneu.jpg)
+
+**Overview**
+
+To easily view our notebook and visualizations, please use the link [here](https://nbviewer.org/github/teigen30/PNAImageClassification/blob/main/PneumoniaXrayClassification.ipynb)
+
+This project analyses over 5,800 chest x-ray images taken of pediatric patients both with and without pneumonia and develops a binary classification model to predict whether a patient has pneumonia or not. The JPEG images were selected from groups of pediatric patients ages one to five years old from Guangzhou Women and Children's Medical Center, Guangzhou China. The World Health Organization (WHO) is requesting a model to help accurately predict pneumonia in young patients. 
+
+**Business and Data Understanding**
+
+Pneumonia is the single largest infectious cause of death in children worldwide and results in 22% of all deaths in children aged 1 to 5. Early identification and intervention is key in reducing the death rate of children with pneumonia. WHO will use the model to help identify the early stages of pneumonia in children, therefore giving providers sufficient time to administer medication in order to ameliorate the patient’s illness. When building the model, careful consideration was taken to identify and remedy false negative pneumonia results. A false negative in this case incorrectly identifies the patient as being healthy when they have pneumonia. False negatives are the most dangerous as they allow the illness to progress in the absence of medical intervention.
+
+Our dataset is provided by Mendeley Data and uses over 5800 chest x-ray images of pediatric patients ages one to five from Guangzhou Women and Children's Medical Center. The images were taken as part of the patients’ routine care. The images were analysed by two expert physicians before being cleared for use in training the AI system and a third expert validated the analysis.
+
+**Stakeholder Audience**
+
+The stakeholder for this proposal is the World Health Organization (WHO). WHO’s mission statement includes their goal of resolving the human resources for health crisis. To help meet that goal, our project aims at providing a model to identify pneumonia in x-ray images without the help of an experienced radiologist, the doctor that would typically assessing the x-ray images and providing the results. This makes pneumonia diagnosis more accessible to people all over the world, specifically in isolated areas with little to know medical staff nearby. Additionally, the model will help to reduce costs, as it cuts out the work of the experienced doctor needed to evaluate the x-rays.
+
+**Modeling**
+
+CNN
+
+Initial Exploration: 
+
+A separate validation set of images was created due to the small size of the provided validation set (16 images).  The folder of pneumonia verified x-ray images was just under four times larger than the folder for healthy x-rays and will be dealt with by transforming the normal images to increase the number of them in future model iterations. Due to computational limitations, images were reduced from an average height/width of 968 x 1320 pixels to 255 x 255 pixels. A test train split was used to separate image data into test, train, and validation (holdout) folders.
+![training_image_count](images/train_image_count.jpg)
+
+Baseline Model: 
+
+The baseline model was a simple CNN consisting of one flatten and two dense layers. The baseline model resulted in a test loss of 0.6862 and a test accuracy of 0.6259.
+
+Final Model: 
+
+The final model (Model 5) was a more complex CNN made up of nine layers. The CNN contained Dense (3), Conv2D (2), Flatten (1), Dropout (1), and Average Pooling (2) layers. The model had a precision score of 0.8026, recall of 0.9616, accuracy at 0.8280, and a loss of 0.3594. 
+
+**Model Results**
+
+Parameter Tuning: 
+
+Made adjustments to prevent overfitting using Early Stopping and Dropout. Made sure all aspects of our model were set up to work in a binary classification model. 
+
+Recommendations: 
+
+Our recommendation to WHO is to deploy our model, or one similar, in locations where there are few experts in x-ray image analysis or where pneumonia has proven to be a consistent issue. By doing so, WHO could help diagnose and treat individuals for pneumonia who otherwise would likely not be examined for weeks if at all.
+
+**Conclusion**
+
+A CNN was created that correctly identified x-ray images as having pneumonia 100% of the time and identified Normal images 74% of the time with an overall accuracy of 78%.
+
+Future Work: 
+
+We would like to make improvements in our current model by addressing the imbalance between normal and pneumonia images. The model trained very well on pneumonia images but the lack of normal images is holding our model back.
+
+We would like to make a tool that deploys our model easily for common use, either on a website or an aplplication.
+
+In future work, we would like to create a model that would be able to identify the type of pneumonia (viral/bacterial) present in the patient as treatments for the two types vary. Additionally, we would like to create a similar model using audio recordings of an individual’s breathing to identify the same pneumonia. In areas with low access to medical resources, many doctors detect pneumonia by listening to a patients breathing. Making this model would extend the potential reach and impact of our work by enabling individuals with no access to x-ray technology to be diagnosed as well. We would also like to perform an air quality survery to pinpoint areas with low air quality that could be increasing the risk of pneumonia in children.
+
+**Presentation Link:** 
+
+[Presentation Link](https://www.canva.com/design/DAEv_SZ5S5s/4oebG7_dX6h5wyX9MICMgw/view?utm_content=DAEv_SZ5S5s&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink)
+
+**Repository Navigation**
+
+```
+├── README.md                                   <- The top-level README for reviewers of this project. 
+├── Jen                                         <- Notebook containing work from Jen S
+├── Jennifer C                                  <- Notebook containing work from Jennifer C
+├── PneumoniaXrayClassification.ipynb           <- Final combined notebook with EDA, Visualizations, Explanations, Model Iterations, and our Final Model. 
+├── images                                      <- Contains images used for README.md and notebook. 
+├── presentation.pdf                            <- PDF of the Canva presentation. 
+├── utils.py                                    <- Helpful functions to use in our final notebook
+├── utils2.py                                   <- Helpful functions to use in our final notebook
+├── utils3.py                                   <- Helpful functions to use in our final notebook
+└── utils20.py                                  <- Helpful functions to use in our final notebook
+```
---
+++ utils.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
+import keras
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input, LSTM
+from keras.datasets import mnist
+from keras import regularizers, initializers, optimizers
+import os
+import datetime
+from keras.wrappers.scikit_learn import KerasRegressor
+from tensorflow.keras.callbacks import EarlyStopping
+from pathlib import Path
+import imagesize
+
+def pic_count(path, folder_name):
+    ''' Returns a visualization of the picture counts for image folders by type.
+    
+    path: The path to the folder containing the images. 
+    folder_name: name of the folder as a string. This will set the title of the visualization. 
+    
+    '''
+    dict = {'Normal' :len(os.listdir(path + '/NORMAL')), 'Pneumonia':len(os.listdir(path + '/PNEUMONIA'))}
+    fig = px.bar(dict.keys(), 
+                 dict.values(), 
+                 title=folder_name, 
+                 color=list(dict.keys()), 
+                 orientation='h', 
+                 color_discrete_sequence=px.colors.sequential.ice_r, 
+                 template='plotly_dark', 
+                 width=800,
+                 height=500)
+    fig.show()
+    
+def eda(data, title1, title2, title3): 
+    '''Returns three Plotly visuals including: 
+    
+    1. Scatter plot of Height and Width (pixels)
+    2. Histogram of distrubition Height vs Width, 
+    3. Histogram of distribution of Width vs Height.
+    
+    data: DataFrame with a Height, Width, and Type column. 
+    title1: title of first scatter plot. 
+    title2: title of first histogram. 
+    title3: title of second histogram. 
+    
+    '''
+    fig = px.scatter(data, 
+                     x='Width', 
+                     y='Height',
+                     color='Type',
+                     color_discrete_sequence=px.colors.sequential.ice_r,
+                     template='plotly_dark',
+                     opacity=.4,
+                     title=title1
+                     )
+
+    fig2 = px.histogram(train_img_sizes_df, 
+                        x='Width', 
+                        y='Height',  
+                        color='Type',
+                        color_discrete_sequence=
+                        px.colors.sequential.ice_r, 
+                        template='plotly_dark',
+                        title=title2
+                       )
+
+    fig3 = px.histogram(train_img_sizes_df, 
+                        x='Height', 
+                        y='Width',
+                        color='Type',
+                        color_discrete_sequence=
+                        px.colors.sequential.ice_r, 
+                        template='plotly_dark',
+                        title=title3
+                       )
+    fig.show()
+    fig2.show()
+    fig3.show()
+    
+def images_to_df(root1_, root2_, image_type1, image_type2):
+    """ Returns a concatenated DataFrame containing columns for FileName, Size, Width, Aspect Ratio, and Image Type.
+    Some of this code was taken from https://medium.com/analytics-vidhya/how-to-pick-the-optimal-image-size-for-training-convolution-neural-network-65702b880f05 that grabs the Width and Height of the images. 
+    
+    root1: first path to image folder.
+    root2: second path to image folder. 
+    image_type1: Image Type from the first folder.
+    image_type2: Image Type from the second folder.
+    
+    """ 
+    imgs1 = [img.name for img in Path(root1_).iterdir() if img.suffix == ".jpeg"]
+    img_meta1 = {}
+    for f in imgs1: img_meta1[str(f)] = imagesize.get(root1_+f)
+
+    # Convert it to Dataframe and compute aspect ratio
+    data1 = pd.DataFrame.from_dict([img_meta1]).T.reset_index().set_axis(['FileName', 'Size'], axis='columns', inplace=False)
+    data1[["Width", "Height"]] = pd.DataFrame(data1["Size"].tolist(), index=data1.index)
+    data1["Aspect Ratio"] = round(data1["Width"] / data1["Height"], 2)
+    data1["Type"] = image_type1
+    
+    #same for second root
+    imgs2 = [img.name for img in Path(root2_).iterdir() if img.suffix == ".jpeg"]
+    img_meta2 = {}
+    for f in imgs2: img_meta2[str(f)] = imagesize.get(root2_+f)
+
+    # Convert it to Dataframe and compute aspect ratio
+    data2 = pd.DataFrame.from_dict([img_meta2]).T.reset_index().set_axis(['FileName', 'Size'], axis='columns', inplace=False)
+    data2[["Width", "Height"]] = pd.DataFrame(data2["Size"].tolist(), index=data2.index)
+    data2["Aspect Ratio"] = round(data2["Width"] / data2["Height"], 2)
+    data2["Type"] = image_type2
+
+    #Concat DataFrames
+    data = pd.concat([data1, data2], axis=0)
+    
+    return data
+    
+def visualize_training_results(history):
+    '''
+    From https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+    
+    Input: keras history object (output from trained model)
+    '''
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+    fig.suptitle('Model Results')
+
+    # summarize history for accuracy
+    ax1.plot(history.history['accuracy'])
+    ax1.plot(history.history['val_accuracy'])
+    ax1.set_ylabel('Accuracy')
+    ax1.legend(['train', 'test'], loc='upper left')
+    # summarize history for loss
+    ax2.plot(history.history['loss'])
+    ax2.plot(history.history['val_loss'])
+    ax2.set_ylabel('Loss')
+    ax2.legend(['train', 'test'], loc='upper left')
+    
+    plt.xlabel('Epoch')
+    plt.show()
+    
+def plot_performance(hist):
+    """ Returns 4 plots comparing Training and Validation data. 
+    First plot returns training and validation accuracy. 
+    Second plot returns training and validation loss. 
+    Third plot returns training and validation F1-Scores. 
+    Fourth plot returns training and validation recall scores. 
+    
+    hist: input history model containing train images, labels, and validation data. t"""
+    
+    hist_ = hist.history
+    epochs = hist.epoch
+    
+    plt.plot(epochs, hist_['accuracy'], label='Training Accuracy')
+    plt.plot(epochs, hist_['val_accuracy'], label='Validation Accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    
+    plt.plot(epochs, hist_['loss'], label='Training loss')
+    plt.plot(epochs, hist_['val_loss'], label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    recall = np.array(hist_['recall'])
+    precision = np.array(hist_['precision'])
+    val_recall = np.array(hist_['val_recall'])
+    val_precision = np.array(hist_['val_precision'])
+    plt.figure()
+    
+    plt.plot(epochs, 
+             2*((recall * precision)/(recall + precision)), 
+             label='Training f1')
+    plt.plot(epochs, 
+             2*((val_recall * val_precision)/(val_recall + val_precision)), 
+             label='Validation f1')
+    plt.title('Training and validation F1-Score')
+    plt.legend()
+    plt.figure()
+    
+    plt.plot(epochs, recall, label = "Training Recall")
+    plt.plot(epochs, val_recall, label = "Validation Recall")
+    plt.title("Training and Validation Recall Scores")
+    plt.legend()
+    plt.figure()
+    
+    plt.show()
+    
+def confusion_matrix(model, x, y):
+    """Returns a confusion matrix in Seaborn heatmap style.
+    
+    model: model 
+    x: X_val (X validation) 
+    y: y_val (y validation)
+    
+    """
+    y_predict_test = model.predict(x)
+    y_true_test = y
+    res_test = tf.math.confusion_matrix(y_true_test, y_predict_test)
+    res_test
+    
+    return sns.heatmap(res_test, annot=True, fmt='g')
+
+
+
+    
